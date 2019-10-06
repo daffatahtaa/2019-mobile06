d5686bf9ffd560fb3d82064b3eeb088719652648
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-10-06 20:54:40 +0700

    praktikum bundle

--- app/src/main/java/id/ac/polinema/idealbodyweight/fragments/AboutFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragments/AboutFragment.java
@@ -1,9 +1,9 @@
 package id.ac.polinema.idealbodyweight.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,30 +11,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import id.ac.polinema.idealbodyweight.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AboutFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link AboutFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AboutFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    //private static final String ARG_PARAM1 = "param1";
-    //private static final String ARG_PARAM2 = "param2";
     private static final String ARG_NAME = "name";
+
 
     // TODO: Rename and change types of parameters
     private String name;
-    //private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
     public AboutFragment() {
         // Required empty public constructor
@@ -49,7 +41,7 @@ public class AboutFragment extends Fragment {
      * @return A new instance of fragment AboutFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
+    public static AboutFragment newInstance(String name) {
         AboutFragment fragment = new AboutFragment();
         Bundle args = new Bundle();
         args.putString(ARG_NAME, name);
@@ -58,11 +50,10 @@ public class AboutFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             name = getArguments().getString(ARG_NAME);
-            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -70,49 +61,10 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_about, container, false);
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-        TextView nameText = view.findViewsWithText(R.id.text_name);
+        TextView nameText = view.findViewById(R.id.text_name);
         nameText.setText(name);
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
